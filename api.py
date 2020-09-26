f048dd07cbec545bce6037fd84d5f044296ec195
santeive <packo.ive@gmail.com>
2020-09-25 23:32:37 -0500

    SQL Alchemy - ItemModel - UserModel

--- api.py
+++ api.py
@@ -1,14 +1,21 @@
 from flask import Flask
 from flask_restful import Api
 from flask_jwt import JWT
+from db import db
 
 from security import authenticate, identity
 from resources.users import UserRegister
 from resources.item import Item, ItemList
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = '84hisd¡*!_-174gs'
 api = Api(app)
+
+@app.before_first_request
+def create_tables():
+    db.create_all()
 
 #/auth
 jwt = JWT(app, authenticate, identity)
@@ -17,5 +24,7 @@ api.add_resource(Item, '/item/<string:name>')
 api.add_resource(ItemList, '/items')
 api.add_resource(UserRegister, '/register')
 
+
 if __name__ == '__main__':
+    db.init_app(app)
     app.run(port=5000, debug=True)
